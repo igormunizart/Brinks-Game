da87719b48cd5191b4b0f603251e93dba5b4e141
Igor José Muniz <69331668+igormunizart@users.noreply.github.com>
2021-11-10 23:26:57 -0300

    v3

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1270166.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 60086266, 60086267, 60086268, 60086269 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/60086290/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/60086291/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/60086289/1/ammo.js', 'preload' : true},
+];
